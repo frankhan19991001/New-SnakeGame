5998029bc3f9651a39c65df611afd7b99090addb
Frank <franky.han@mail.utoronto.ca>
2024-09-13 11:04:34 -0400

    added features

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.security.Key;
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.swing.*;
 
 public class Main extends JPanel implements KeyListener {
     public static final int CELL_SIZE = 20;
-    public static int width = 400;
-    public static int height = 400;
+    public static final int width = 400;
+    public static final int height = 400;
     public static int row = height / CELL_SIZE;
     public static int column = width / CELL_SIZE;
     private Snake snake;
     private Fruit fruit;
     private Timer t;
-    private Integer speed = 100;
+    private final Integer speed = 100;
     private static String direction;
     private boolean allowKeyPress;
     private int score;
+    private int highestScore;
+
     public Main() {
+        read_highest_score();
         reset();
         addKeyListener(this);
     }
@@ -50,52 +56,15 @@ public class Main extends JPanel implements KeyListener {
 
     @Override
     public void paintComponent(Graphics g) {
-        // check game over
-        var snake_body = snake.getSnakeBody();
-        var head = snake_body.get(0);
-        for (int i = 1; i < snake.getSnakeBody().size(); i++) {
-            if (snake_body.get(i).x == head.x && snake_body.get(i).y == head.y) {
-                allowKeyPress = false;
-                t.cancel();
-                t.purge();
-                int response = JOptionPane.showOptionDialog(
-                        this,
-                        "Restart?",
-                        "Game Over",
-                        JOptionPane.YES_NO_OPTION,
-                        JOptionPane.INFORMATION_MESSAGE,
-                        null,
-                        null, // Options to display
-                        JOptionPane.YES_OPTION // Default option
-                );
-                switch (response) {
-                    case JOptionPane.CLOSED_OPTION, JOptionPane.NO_OPTION:
-                        System.exit(0);
-                        break;
-                    case JOptionPane.YES_OPTION:
-                        reset();
-                        return;
-                }
-            }
+        if (gameOverLogic()) {
+            return;
         }
-        // black background
-        g.fillRect(0, 0, width, height);
-        fruit.drawFruit(g);
-        snake.drawSnake(g);
+
+        draw_basic_object_and_environment(g);
 
         // remove snake tail and put at head
         var newHead = getNewHead();
-
-        if (snake.getSnakeBody().get(0).x == fruit.getX()
-                && snake.getSnakeBody().get(0).y == fruit.getY()) {
-            fruit.setNewLocation(snake);
-            fruit.drawFruit(g);
-            // score++
-        } else {
-            snake.getSnakeBody().remove(snake.getSnakeBody().size() - 1);
-        }
-
-        snake.getSnakeBody().add(0, newHead);
+        drawSnakeAndFruit(g, newHead);
 
         allowKeyPress = true;
         requestFocusInWindow();
@@ -104,18 +73,38 @@ public class Main extends JPanel implements KeyListener {
     private Node getNewHead() {
         int snakeX = snake.getSnakeBody().get(0).x;
         int snakeY = snake.getSnakeBody().get(0).y;
-        if(direction.equals("Left")) {
-            snakeX -= CELL_SIZE;
-        } else if(direction.equals("Up")) {
-            snakeY -= CELL_SIZE;
-        } else if(direction.equals("Right")) {
-            snakeX += CELL_SIZE;
-        } else if(direction.equals("Down")) {
-            snakeY += CELL_SIZE;
+        switch (direction) {
+            case "Left":
+                snakeX -= CELL_SIZE;
+                break;
+            case "Up":
+                snakeY -= CELL_SIZE;
+                break;
+            case "Right":
+                snakeX += CELL_SIZE;
+                break;
+            case "Down":
+                snakeY += CELL_SIZE;
+                break;
+            default:
+                break;
         }
 
         Node newHead = new Node(snakeX, snakeY);
         return newHead;
+    }
+
+    private void drawSnakeAndFruit(Graphics g, Node newHead) {
+        if (snake.getSnakeBody().get(0).x == fruit.getX()
+                && snake.getSnakeBody().get(0).y == fruit.getY()) {
+            fruit.setNewLocation(snake);
+            fruit.drawFruit(g);
+            score++;
+        } else {
+            snake.getSnakeBody().remove(snake.getSnakeBody().size() - 1);
+        }
+        
+        snake.getSnakeBody().add(0, newHead);
     }
 
     @Override
@@ -158,5 +147,93 @@ public class Main extends JPanel implements KeyListener {
     @Override
     public void keyReleased(KeyEvent keyEvent) {
 
+    }
+
+    private void read_highest_score() {
+        try {
+            File myObj = new File("filename.txt");
+            try (Scanner myReader = new Scanner(myObj)) {
+                highestScore = myReader.nextInt();
+            }
+        } catch (FileNotFoundException e) {
+            highestScore = 0;
+            try {
+                File myObj = new File("filename.txt");
+                if (myObj.createNewFile()) {
+                    System.out.println("File created " + myObj.getName());
+                }
+                try (FileWriter myWriter = new FileWriter(myObj.getName())) {
+                    myWriter.write("" + 0);
+                }
+            } catch(IOException err) {
+                System.out.println("An error occured");
+                err.printStackTrace();
+            }
+        }
+    }
+
+    public void write_files(int score) {
+        try {
+            try (FileWriter myWriter = new FileWriter("filename.txt")) {
+                if (score > highestScore) {
+                    myWriter.write("" + score);
+                    highestScore = score;
+                } else {
+                    myWriter.write(highestScore);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean gameOverLogic() throws HeadlessException {
+        // check game over
+        var snake_body = snake.getSnakeBody();
+        var head = snake_body.get(0);
+        for (int i = 1; i < snake.getSnakeBody().size(); i++) {
+            if (snake_body.get(i).x == head.x && snake_body.get(i).y == head.y) {
+                allowKeyPress = false;
+                t.cancel();
+                t.purge();
+                int response = JOptionPane.showOptionDialog(
+                        this,
+                        "Restart?",
+                        "Game Over",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.INFORMATION_MESSAGE,
+                        null,
+                        null, // Options to display
+                        JOptionPane.YES_OPTION // Default option
+                );
+                write_files(score);
+                switch (response) {
+                    case JOptionPane.CLOSED_OPTION:
+                        System.exit(0);
+                        break;
+                    case JOptionPane.NO_OPTION:
+                        System.exit(0);
+                        break;
+                    case JOptionPane.YES_OPTION:
+                        reset();
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void draw_basic_object_and_environment(Graphics g) {
+        // black background
+        g.fillRect(0, 0, width, height);
+        fruit.drawFruit(g);
+        snake.drawSnake(g);
+        
+        // draw score
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 12));
+        g.drawString("Score: " + score, 5, 15);
+        g.setColor(Color.YELLOW);
+        g.drawString("High Score: " + highestScore, 5, 30);
     }
 }
